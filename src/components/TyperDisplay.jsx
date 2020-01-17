b01889a057928ec123006030d32be0a5eb0daabb
DaffaFA <daffafa562@gmail.com>
2020-01-17 08:40:28 +0700

    add wrong text warning

--- src/components/TyperDisplay.jsx
+++ src/components/TyperDisplay.jsx
@@ -1,14 +1,14 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { TyperContext } from '../contexts/TyperContext';
 
 
-const TyperText = ({word, wordId, currentId, isWrong}) => (
-    <span className={wordId === currentId ? isWrong ? 'active danger' : 'active' : ''}>{ word } </span>
+const TyperText = ({ word, wordId, currentId, isWrong, wrongList }) => (
+    <span><span className={(wordId === currentId ? isWrong ? 'active danger' : 'active' : '') + (wrongList.includes(wordId) && wordId < currentId ? 'danger' : '') + (!wrongList.includes(wordId) && wordId < currentId ? 'primary' : '')}>{word}</span> </span>
 )
 
 const TyperDisplay = () => {
-    const { display: { keyword, textId, isWrong } } = useContext(TyperContext);
-    return keyword.map((word, id) => <TyperText key={id} isWrong={isWrong} currentId={textId} wordId={id} word={word} />)
+    const { display: { keyword, textId, isWrong, wrongList } } = useContext(TyperContext);
+    return keyword.map((word, id) => <TyperText key={id} isWrong={isWrong} wrongList={wrongList} currentId={textId} wordId={id} word={word} />)
 }
 
 export default TyperDisplay;
--- src/contexts/TyperContext.jsx
+++ src/contexts/TyperContext.jsx
@@ -4,21 +4,9 @@ export const TyperContext = createContext();
 
 const TyperContextProvider = ({ children }) => {
   const [text, setText] = useState("");
-
   const [textId, setTextId] = useState(0);
-
   const [isWrong, setIsWrong] = useState(false);
-
-  const checkKeyword = () => {
-    setIsWrong(!keyword[textId].includes(text));
-    console.log(keyword[textId], isWrong)
-  };
-
-  const nextKeyword = () => {
-    setText('')
-    setTextId(textId + 1);
-  }
-
+  const [wrongList, setWrongList] = useState([]);
   const keyword = [
     "the",
     "of",
@@ -122,6 +110,22 @@ const TyperContextProvider = ({ children }) => {
     "part"
   ];
 
+  const checkKeyword = () => {
+    if (textId < keyword.length) {
+      setIsWrong(!keyword[textId].includes(text))
+    }
+  };
+
+  const nextKeyword = () => {
+    if (textId < keyword.length) {
+
+      isWrong || text.length !== keyword[textId].length ? setWrongList([...wrongList, textId]) : console.log();
+      setText('')
+      setTextId(textId + 1);
+    }
+    console.log(wrongList);
+  }
+
   let value = {
     type: {
       text,
@@ -132,7 +136,8 @@ const TyperContextProvider = ({ children }) => {
     display: {
       keyword,
       textId,
-      isWrong
+      isWrong,
+      wrongList
     }
   };
 
